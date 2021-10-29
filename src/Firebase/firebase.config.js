22917d0987b1b6f914b7e339342ab9cdfd02487a
MdRifatchimu <himu2411@swfu.edu.cn>
2021-10-29 12:11:20 +0600

    firebase connected

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0F3lbCMFgH_5QVoE8iy9DdsW-5SDh32k",
+
+  authDomain: "the-tour-bees.firebaseapp.com",
+
+  projectId: "the-tour-bees",
+
+  storageBucket: "the-tour-bees.appspot.com",
+
+  messagingSenderId: "472144090925",
+
+  appId: "1:472144090925:web:633617bec555d793e925a4"
+};
+
+export default firebaseConfig;
+
+// console.log(process.env);
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+// };
+
+// export default firebaseConfig;
